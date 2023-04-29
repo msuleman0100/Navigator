0386123363bf449f8d169d9d36201bb0784906c7
msuleman0100 <msuleman0100@gmail.com>
2023-04-29 17:07:14 +0500

    basics project added

---
+++ Navigator/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Navigator
+//
+//  Created by Muhammad Suleman on 4/29/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Navigator/NavigatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigatorApp.swift
+//  Navigator
+//
+//  Created by Muhammad Suleman on 4/29/23.
+//
+
+import SwiftUI
+
+@main
+struct NavigatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
